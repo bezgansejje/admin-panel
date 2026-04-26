14d58eb14eed8d74a234983d0a15aab047eb5024
Kesha <kirill.bezgans04@gmail.com>
2026-04-26 17:11:56 +0500

    Update Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Lakes Admin',
   description: 'Admin panel for lakes-backend',
+  icons: {
+    icon: '/lake_.ico',
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
